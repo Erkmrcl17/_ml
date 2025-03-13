8dddd4291972f16715bf2ae876ce1282007e49a9
Erkmrcl17 <erikachia123@gmail.com>
2025-03-13 11:21:09 +0800

    123

---
+++ HW8.py
@@ -0,0 +1,25 @@
+import torch
+
+x = torch.tensor(0.0, requires_grad=True)
+y = torch.tensor(0.0, requires_grad=True)
+z = torch.tensor(0.0, requires_grad=True)
+
+lr= 0.01
+
+def loss_function(x, y, z):
+    return (x - 1) ** 2 + (y - 2) ** 2 + (z - 3) ** 2  
+
+for _ in range(10000):
+    loss = loss_function(x, y, z)  
+    loss.backward()  
+
+    with torch.no_grad():
+        x -= lr * x.grad
+        y -= lr * y.grad
+        z -= lr * z.grad
+
+    x.grad.zero_()
+    y.grad.zero_()
+    z.grad.zero_()
+
+print(f"x = {x.item():.4f}, y = {y.item():.4f}, z = {z.item():.4f}")
