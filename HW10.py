06c402137ac213718b4bc5d5f3c420446400903a
Erkmrcl17 <erikachia123@gmail.com>
2025-03-28 22:19:45 +0800

    111

--- HW10.py
+++ HW10.py
@@ -1,14 +1,4 @@
 import ollama
 
-def chat_llama(prompt):
-    response = ollama.chat(model='llama3:2.3b', messages=[{'role': 'user', 'content': prompt}])
-    return response['message']['content']
-
-print("Chatbot Llama 3")
-while True:
-    user_input = input("Kamu: ")
-    if user_input.lower() in ['exit', 'quit']:
-        print("Bot: bye!")
-        break
-    response = chat_llama(user_input)
-    print("Bot:", response)
+response = ollama.chat(model='llama3:2.3b', messages=[{'role': 'user', 'content': "what is the delicious food in indonesia"}])
+print(response["message"]["content"])
